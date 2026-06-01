d15ffb455dd498a1bf4548164e7d98e1d9e60950
Abhishek Gupta <abhig3710@gmail.com>
2026-06-01 12:06:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ Routes to the main app in apps/web/app.py
 import subprocess
 import sys
 import os
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parent))
+from apps.web.app import *
 
 # Ensure the app directory is in the Python path
 sys.path.insert(0, os.path.dirname(__file__))
